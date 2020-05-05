64afff6af26454636e446fa7f755be2916941686
geryy <g@geryy.io>
2020-05-05 17:04:36 +0200

    add cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,8 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
