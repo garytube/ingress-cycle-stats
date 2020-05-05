cb5f7d2444150bb8397cb6b60bbd8a4f42c26a37
geryy <g@geryy.io>
2020-05-05 18:01:49 +0200

    add manifest and app icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,18 @@ module.exports = {
     `gatsby-plugin-offline`,
     `gatsby-plugin-netlify-cms`,
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Ingress Cycle Stats",
+        short_name: "Cycles",
+        start_url: "/",
+        background_color: "#000000",
+        theme_color: "#04eaf5",
+        display: "standalone",
+        icon: "src/images/maskable_icon.png",
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `cycles`,
