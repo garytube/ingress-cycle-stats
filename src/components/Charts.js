86a17f3161697515f6e056cf6c68c752797dea75
geryy <g@geryy.io>
2021-01-22 19:54:11 +0100

    chart

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -87,6 +87,8 @@ export function ScoreLine({ data }) {
     scales: {
       xAxes: [
         {
+          type: 'time',
+          distribution: 'series',
           display: true,
           ticks: {
             beginAtZero: false,
@@ -95,13 +97,44 @@ export function ScoreLine({ data }) {
       ],
       yAxes: [
         {
-          display: false,
-          ticks: {
-            beginAtZero: false,
+          gridLines: {
+            display: true,
+            drawBorder: false,
+            drawOnChartArea: true,
+            drawTicks: false,
+            color: '#04e9f51e'
           },
-        },
-      ],
+          ticks: {
+            callback: (value) => number_format(value)
+          },
+          display: true,
+        }]
     },
+  }
+
+  function number_format(number, decimals, dec_point, thousands_sep) {
+    // *     example: number_format(1234.56, 2, ',', ' ');
+    // *     return: '1 234,56'
+    number = (number + '').replace(',', '').replace(' ', '');
+    var n = !isFinite(+number) ? 0 : +number,
+      prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+      sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+      dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+      s = '',
+      toFixedFix = function (n, prec) {
+        var k = Math.pow(10, prec);
+        return '' + Math.round(n * k) / k;
+      };
+    // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+    s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+    if (s[0].length > 3) {
+      s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+    }
+    if ((s[1] || '').length < prec) {
+      s[1] = s[1] || '';
+      s[1] += new Array(prec - s[1].length + 1).join('0');
+    }
+    return s.join(dec);
   }
 
   return (
@@ -109,7 +142,7 @@ export function ScoreLine({ data }) {
       partialVisibility
       onChange={isVisible => setVisable(isVisible)}
     >
-      <ChartSpacer height="420px">
+      <ChartSpacer height="400px">
         {visable && <Line data={foo} options={options} />}
       </ChartSpacer>
     </VisibilitySensor>
